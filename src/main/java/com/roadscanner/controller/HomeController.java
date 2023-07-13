0db882fb14c7f06e65b11c2312f1312f6928db46
ODH <dh103504@naver.com>
2023-07-13 17:49:21 +0900

    test-end

--- src/main/java/com/roadscanner/controller/HomeController.java
+++ src/main/java/com/roadscanner/controller/HomeController.java
@@ -10,5 +10,9 @@ public class HomeController {
     public String index() {
         return "index";
     }
-
+    
+    @GetMapping("/main")
+    public String main() {
+        return "mainmenu";
+    }
 }
