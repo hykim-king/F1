c40064ebfa85ffcd5c0e3de7cdc6a8ef4f25b9f5
pabbajo <chagusrud@gmail.com>
2023-07-13 15:37:52 +0900

    no message

--- src/main/java/com/roadscanner/controller/Hello.java
+++ src/main/java/com/roadscanner/controller/Hello.java
@@ -3,12 +3,9 @@ package com.roadscanner.controller;
 public class Hello {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("hi world kkkkk");
-=======
 
 		System.out.println("hi world kk");
->>>>>>> sr
 
 	}
 
