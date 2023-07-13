973429fca5b459e0503c8e39ff785cd9a3b14c33
KJM <kimjmini1122@gmail.com>
2023-07-13 15:14:38 +0900

    cocommit

--- src/main/java/com/roadscanner/controller/Hello.java
+++ src/main/java/com/roadscanner/controller/Hello.java
@@ -3,8 +3,8 @@ package com.roadscanner.controller;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hi world");
-
+		System.out.println("hi world kkkkk");
+		
 	}
 
 }
