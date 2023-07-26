0ab1359df2f923067651935f192623691cc7d662
정민 <kimjmini1122@gmail.com>
2023-07-26 16:32:36 +0900

    upload.jsp와 controller 생성

--- src/main/java/com/roadscanner/controller/UploadController.java
+++ src/main/java/com/roadscanner/controller/UploadController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class FeedbackController {
+public class UploadController {
 	
-	@GetMapping(value = "/feedback")
+	@GetMapping(value = "/feedback") //http://localhost:8080/road-scanner/feedback
 	public String feedback() {
 		return "feedback";
-//	http://localhost:8080/road-scanner/feedback
+	}
+	
+	@GetMapping(value = "/upload")
+	public String upload() {
+		return "upload";
 	}
 }
