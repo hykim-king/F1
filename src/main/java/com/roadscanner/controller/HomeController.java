7a839073c9f8f524b414c99ffa43e7b4260d6ab1
차지욱 <sitery@naver.com>
2023-07-26 17:39:56 +0900

    import 정리

--- src/main/java/com/roadscanner/controller/HomeController.java
+++ src/main/java/com/roadscanner/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.roadscanner.controller;
 
-import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
